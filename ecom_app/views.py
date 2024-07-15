3a1feb1a5c15e6d0dbdf1a190b36d9515646d8ad
Bhaskar Anand <official.beingbhaskar@gmail.com>
2024-07-15 09:51:59 +0530

    Show categories and active category, implement search function

--- ecom_app/views.py
+++ ecom_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from products.models import *
+from django.db.models import Q
 # Create your views here.
 def frontpage(request):
     products=Product.objects.all()[0:8]
@@ -7,5 +8,21 @@ def frontpage(request):
 
 def shop(request):
     products=Product.objects.all()
-    return render(request,'ecom_app/shop.html', context={'products':products})
+    categories=Category.objects.all()
+    active_category=request.GET.get('category','')
+
+    if active_category:
+        products=products.filter(prod_category__slug=active_category)
+
+    query= request.GET.get('query','')
+
+    if query:
+        products=products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    context={
+        'products': products,
+        'categories':categories,
+        'active_category':active_category,
+    }
+    return render(request,'ecom_app/shop.html', context)
 
